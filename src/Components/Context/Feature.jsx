506b2cc2f435a739f369330f5f7eb0512b59f95d
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-11-10 23:05:36 +0500

    first commist

--- src/Components/Context/Feature.jsx
+++ src/Components/Context/Feature.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
+import Slider from "react-slick";
 
 const Feature = () => {
-  return (
-    <div className='feature'>
-      <h1>Feature Categories</h1>
-    </div>
-  )
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+    return (
+        
+        <div className='feature'>
+            <h1>Feature Categories</h1>
+            <div className="slider-container">
+        <Slider {...settings}>
+            
+        </Slider>
+        </div>
+        </div>
+        
+    )
 }
 
 export default Feature
