ceaff2067260f63db28f06a89b6734f5d1a1ae54
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-11-16 19:25:28 +0500

    first com

--- src/Components/Header/DualBanner.jsx
+++ src/Components/Header/DualBanner.jsx
@@ -5,10 +5,10 @@ const DualBanner = () => {
   return (
     <div className='Double'>
       <div className='img-1'>
-      <img src="/assets/img/Dualbannerimg1.png" alt="" />
+      <img src="/assets/img/Dualbannerimg1.png" alt="" className='pic'/>
       </div>
       <div className='img-2'>
-      <img src="/assets/img/Dualbannerimg2.png" alt="" />
+      <img src="/assets/img/Dualbannerimg2.png" alt="" className='pic'/>
       </div>
     </div>
   )
